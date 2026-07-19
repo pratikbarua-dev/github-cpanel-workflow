fe34d688cb8d38866ccdc6e59e45d08f8924d35e
Pro-Tik <baruaprathik06@gmail.com>
2026-07-19 12:33:12 +0600

    fix: resolve syntax error in xml escape function

--- src/controllers/publicController.js
+++ src/controllers/publicController.js
@@ -611,7 +611,7 @@ exports.getSitemap = async (req, res) => {
                     case '<': return '&lt;';
                     case '>': return '&gt;';
                     case '&': return '&amp;';
-                    case '\\'': return '&apos;';
+                    case "'": return '&apos;';
                     case '"': return '&quot;';
                 }
             });
